d174ce3a82e676f67ae6b4548c9cc945c7a90963
陳 亮宇 <fredchen000@hotmail.com>
2018-02-11 20:23:29 +0800

    Initial project for comp3111

---
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -0,0 +1,11 @@
+package labs.comp3111.ust.hk;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lab 1: Introduction to Git and GitHub");
+
+	}
+
+}
