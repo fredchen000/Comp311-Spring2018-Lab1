ab8aad903d92747f665fb5fb65064ceb26379268
陳 亮宇 <fredchen000@hotmail.com>
2018-02-11 20:24:48 +0800

    first modify

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -5,7 +5,7 @@ public class Lab1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lab 1: Introduction to Git and GitHub");
-
+		System.out.println("Successfully learn git basic");
 	}
 
 }
